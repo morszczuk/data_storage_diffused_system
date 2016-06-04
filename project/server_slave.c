9cf7e968cc3cb2936285d1c0b05881659348be30
Miron Marczuk <miron.marczuk@gmail.com>
2016-06-04 19:33:49 +0200

    Project initated

---
+++ project/server_slave.c
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
